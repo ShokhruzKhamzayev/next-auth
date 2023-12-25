8a3e335254deb5ebab49de45599a86cba118dfa2
Shoxruz <forpk432@gmail.com>
2023-12-26 00:24:03 +0500

    changing_a_few_styles

--- src/app/sign-in/[[...sing-in]]/page.jsx
+++ src/app/sign-in/[[...sing-in]]/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { SignIn } from '@clerk/nextjs'
+import { metadata } from '../../layout.js'
 
 export default function Page() {
+    metadata.title = 'Sign In | Shokhruz '
     return (
         <div className='flex justify-center items-center min-h-screen'>
             <SignIn />
--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { SignUp } from '@clerk/nextjs'
+import {metadata} from '../../layout'
 
 export default function Page() {
+    metadata.title = 'Sign Up | Shokhruz'
     return (
         <div className='flex justify-center items-center min-h-screen'>
             <SignUp />
